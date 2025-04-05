ce0a3ad9327c60669855a5ed0b0a29b004adc3a7
Knet Solutions <knetsolutions-dev@yahoo.com>
2025-04-05 12:58:58 +0530

    Update project_generator.py

--- app/project_generator.py
+++ app/project_generator.py
@@ -6,20 +6,16 @@ class ProjectGenerator:
         self.workspace_directory = os.path.join("Workspace", self.app_name)
     
     def create_project_structure(self, project_structure):
-        # Create the root directory
         os.makedirs(self.workspace_directory, exist_ok=True)
         
-        # Create the directories and files based on project_structure
         for line in project_structure.split("\n"):
             line = line.strip()
             if line.endswith("/"):
-                # It's a directory
                 directory_path = os.path.join(self.workspace_directory, line)
                 os.makedirs(directory_path, exist_ok=True)
             else:
-                # It's a file
                 file_path = os.path.join(self.workspace_directory, line)
                 with open(file_path, 'w') as file:
-                    pass  # Create an empty file
+                    pass
 
         print(f"Project structure for {self.app_name} created successfully.")
